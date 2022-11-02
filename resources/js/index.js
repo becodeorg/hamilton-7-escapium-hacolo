58b012bfbe7216b51eebac06f545ed530df1684c
Loïc <hloic@outlook.com>
2022-11-02 18:57:57 +0100

    single post html ok left aside

--- resources/js/index.js
+++ resources/js/index.js
@@ -63,3 +63,16 @@ function createElement(type, parent, classList) {
     parent.append(element);
     return element;
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
